e381267204711bf1259c584145ef77fbde7bc979
Amar AA <amar.ashfaq95@gmail.com>
2025-03-02 02:38:08 +0000

    Added user input feature

--- demoProject/Program.cs
+++ demoProject/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace demoProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Demo Project!");
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Hello, {name}!");
+        }
+    }
+}
